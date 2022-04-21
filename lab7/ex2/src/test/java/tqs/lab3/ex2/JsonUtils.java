559fe9a262eff9dff52eb0ec3035dc64cf96b081
Martinho Tavares <martinho.tavares@ua.pt>
2022-04-21 16:05:21 +0100

    Lab7: Ex1 done, Ex2 started

---
+++ lab7/ex2/src/test/java/tqs/lab3/ex2/JsonUtils.java
@@ -0,0 +1,11 @@
+package tqs.lab3.ex2;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JsonUtils {
+
+    public static final Gson gson = new GsonBuilder()
+            .create();
+
+}
